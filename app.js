cbb357895e6aac1af983ac37268dc1b9e107c97c
martijnkunstman <martijn@kunstman.nl>
2020-01-10 20:31:16 +0100

    speed and center

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000;
+const port = 8080;
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 app.use(express.static('public'))
 
@@ -9,13 +9,17 @@ const io = socket(server)
 
 let gameData = []
 let food = "";
+let initialSnakeSpeed = 200;
+let snakeSpeedGrow = 20;
+let snakeSpeedMin = 100;
+let time = new Date().getTime();
 const gridSize = 16
-const speed = 100
+const speed = 1000 / 60;
 
 io.on('connection', newConnection)
 
 function newConnection(socket) {
-   gameData.push({ "id": socket.id, "food": "", "direction": 0, "length": 1, "snake": [getRandomEmptyPosition()] });
+   gameData.push({ "id": socket.id, "time": time, "speed": initialSnakeSpeed, "direction": 0, "length": 1, "snake": [getRandomEmptyPosition()] });
    //console.log("newConnection:" + socket.id)
    socket.emit('init', gridSize)
    socket.on('disconnect', function () {
@@ -33,39 +37,45 @@ function newConnection(socket) {
 }
 
 function tick() {
+   time = new Date().getTime();
    //update snake positions
    for (var i = 0; i < gameData.length; i++) {
-      //update snake position
-      currentX = gameData[i].snake[gameData[i].snake.length - 1].x;
-      currentY = gameData[i].snake[gameData[i].snake.length - 1].y;
-      currentDirection = gameData[i].direction;
-      if (currentDirection == 1) {
-         currentY--;
-         if (currentY < 0) {
-            currentY = gridSize - 1;
+
+
+      if (time - gameData[i].time > gameData[i].speed) {
+         //update snake position only if time difference is bigger than snake speed
+         currentX = gameData[i].snake[gameData[i].snake.length - 1].x;
+         currentY = gameData[i].snake[gameData[i].snake.length - 1].y;
+         currentDirection = gameData[i].direction;
+         if (currentDirection == 1) {
+            currentY--;
+            if (currentY < 0) {
+               currentY = gridSize - 1;
+            }
          }
-      }
-      if (currentDirection == 2) {
-         currentY++;
-         if (currentY > gridSize - 1) {
-            currentY = 0;
+         if (currentDirection == 2) {
+            currentY++;
+            if (currentY > gridSize - 1) {
+               currentY = 0;
+            }
          }
-      }
-      if (currentDirection == 3) {
-         currentX--;
-         if (currentX < 0) {
-            currentX = gridSize - 1;
+         if (currentDirection == 3) {
+            currentX--;
+            if (currentX < 0) {
+               currentX = gridSize - 1;
+            }
          }
-      }
-      if (currentDirection == 4) {
-         currentX++;
-         if (currentX > gridSize - 1) {
-            currentX = 0;
+         if (currentDirection == 4) {
+            currentX++;
+            if (currentX > gridSize - 1) {
+               currentX = 0;
+            }
          }
-      }
-      gameData[i].snake.push({ "x": currentX, "y": currentY });
-      if (gameData[i].snake.length > gameData[i].length) {
-         gameData[i].snake.shift();
+         gameData[i].snake.push({ "x": currentX, "y": currentY });
+         gameData[i].time = time;
+         if (gameData[i].snake.length > gameData[i].length) {
+            gameData[i].snake.shift();
+         }
       }
    }
    // set food
@@ -77,12 +87,16 @@ function tick() {
       if (gameData[i].snake[gameData[i].snake.length - 1].x == food.x && gameData[i].snake[gameData[i].snake.length - 1].y == food.y) {
          food = getRandomEmptyPosition();
          gameData[i].length++;
+         gameData[i].speed = gameData[i].speed - snakeSpeedGrow;
+         if (gameData[i].speed < snakeSpeedMin) {
+            gameData[i].speed = snakeSpeedMin;
+         }
          break;
       }
    }
 
    //emit gameData
-   io.emit('gameData', [gameData, food])
+   io.emit('gameData', [gameData, food, time])
    setTimeout(tick, speed);
 }
 tick();
@@ -99,8 +113,8 @@ function getRandomEmptyPosition() {
                   place.pop();
                }
             }
-         }  
-         if (food != "") { 
+         }
+         if (food != "") {
             if (food.x == a && food.y == b) {
                place.pop();
             }
--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ let gameData;
 let food;
 let direction;
 //--------------------------------------
-socket = io.connect('http://localhost:3000');
+socket = io.connect('http://localhost:8080');
 //--------------------------------------
 socket.on("init", init);
 function init(data) {
@@ -22,7 +22,7 @@ function render(data) {
     cells[i].className = "cell";
   }
   // draw food
-  if (food !="") {
+  if (food != "") {
     document.getElementById("pos-" + food.y + "-" + food.x).classList.add("food");
   }
   // draw all snakes
@@ -30,11 +30,21 @@ function render(data) {
     //draw a snake
     for (var j = 0; j < gameData[i].snake.length; j++) {
       document.getElementById("pos-" + gameData[i].snake[j].y + "-" + gameData[i].snake[j].x).classList.add("body");
+      if (gameData[i].id==socket.id)    
+      {
+        document.getElementById("pos-" + gameData[i].snake[j].y + "-" + gameData[i].snake[j].x).classList.add("green");
+      }
     }
   }
 }
 
 //------------------------------------
+function createGridNew() {
+
+}
+
+//------------------------------------
+
 function createGrid() {
   let br = document.createElement("br");
   for (let a = 0; a < gridsize; a++) {
@@ -42,15 +52,16 @@ function createGrid() {
       let cell = document.createElement("div");
       cell.setAttribute("id", "pos-" + a + "-" + b);
       cell.setAttribute("class", "cell");
-      document.body.appendChild(cell);
+      document.getElementById("game").appendChild(cell);
     }
     let br = document.createElement("br");
-    document.body.appendChild(br);
+    document.getElementById("game").appendChild(br);
   }
   document.addEventListener("keydown", keydown);
 }
 
 //------------------------------------
+
 function keydown(e) {
   if (e.code == "ArrowUp") {
     direction = 1;
@@ -67,195 +78,4 @@ function keydown(e) {
   socket.emit('direction', direction);
 }
 
-
 //------------------------------------
-// to server
-//------------------------------------
-/*
-function tick() {
-  // set position
-  if (direction == 1) {
-    posy--;
-    if (posy < 0) {
-      posy = gridsize - 1;
-    }
-  }
-  if (direction == 2) {
-    posy++;
-    if (posy > gridsize - 1) {
-      posy = 0;
-    }
-  }
-  if (direction == 3) {
-    posx--;
-    if (posx < 0) {
-      posx = gridsize - 1;
-    }
-  }
-  if (direction == 4) {
-    posx++;
-    if (posx > gridsize - 1) {
-      posx = 0;
-    }
-  }
-  let element = "0";
-  if (directionOld != direction) {
-    if (direction == 1) {
-      if (directionOld == 3) {
-        element = "c";
-      }
-      if (directionOld == 4) {
-        element = "b";
-      }
-    }
-    if (direction == 2) {
-      if (directionOld == 3) {
-        element = "a";
-      }
-      if (directionOld == 4) {
-        element = "d";
-      }
-    }
-    if (direction == 3) {
-      if (directionOld == 1) {
-        element = "n";
-      }
-      if (directionOld == 2) {
-        element = "l";
-      }
-    }
-    if (direction == 4) {
-      if (directionOld == 1) {
-        element = "k";
-      }
-      if (directionOld == 2) {
-        element = "m";
-      }
-    }
-  } else {
-    if (direction == 1) {
-      element = "v";
-    }
-    if (direction == 2) {
-      element = "w";
-    }
-    if (direction == 3) {
-      element = "h";
-    }
-    if (direction == 4) {
-      element = "i";
-    }
-  }
-  poshistory.push([posy + "-" + posx, element]);
-  directionOld = direction;
-
-  if (food == "") {
-    setNewFood();
-  }
-  // clear all cells
-  let cells = document.getElementsByClassName("cell");
-  for (let i = 0; i < cells.length; i++) {
-    cells[i].className = "cell";
-  }
-  // create snake
-  for (let i = 0; i < poshistory.length; i++) {
-    document.getElementById(poshistory[i][0]).classList.add("body");
-    if (i == 0) {
-      if (poshistory.length > 1) {
-        if (
-          poshistory[i + 1][1] == "a" ||
-          poshistory[i + 1][1] == "d" ||
-          poshistory[i + 1][1] == "w"
-        ) {
-          document.getElementById(poshistory[i][0]).classList.add("e");
-        }
-        if (
-          poshistory[i + 1][1] == "b" ||
-          poshistory[i + 1][1] == "c" ||
-          poshistory[i + 1][1] == "v"
-        ) {
-          document.getElementById(poshistory[i][0]).classList.add("f");
-        }
-        if (
-          poshistory[i + 1][1] == "k" ||
-          poshistory[i + 1][1] == "m" ||
-          poshistory[i + 1][1] == "i"
-        ) {
-          document.getElementById(poshistory[i][0]).classList.add("g");
-        }
-        if (
-          poshistory[i + 1][1] == "l" ||
-          poshistory[i + 1][1] == "n" ||
-          poshistory[i + 1][1] == "h"
-        ) {
-          document.getElementById(poshistory[i][0]).classList.add("j");
-        }
-      } else {
-        document.getElementById(poshistory[i][0]).classList.add("r");
-      }
-      document.getElementById(poshistory[i][0]).classList.add(poshistory[i][1]);
-    } else {
-      if (i < poshistory.length - 1) {
-        document
-          .getElementById(poshistory[i][0])
-          .classList.add(poshistory[i + 1][1]);
-      } else {
-        if (direction == 1) {
-          document.getElementById(poshistory[i][0]).classList.add("e");
-        }
-        if (direction == 2) {
-          document.getElementById(poshistory[i][0]).classList.add("f");
-        }
-        if (direction == 3) {
-          document.getElementById(poshistory[i][0]).classList.add("g");
-        }
-        if (direction == 4) {
-          document.getElementById(poshistory[i][0]).classList.add("j");
-        }
-      }
-    }
-  }
-  // check snake collision
-  for (let i = 0; i < poshistory.length - 1; i++) {
-    if (poshistory[i][0] == posy + "-" + posx) {
-      document.getElementById(poshistory[i][0]).classList.add("food");
-    }
-  }
-
-  // check food collision
-  if (food == posy + "-" + posx) {
-    snakelength++;
-    speed = speed - 5;
-    if (speed < 100) {
-      speed = 100;
-    }
-    setNewFood();
-  } else {
-    if (poshistory.length > snakelength) {
-      poshistory.shift();
-    }
-  }
-  document.getElementById(food).classList.add("food");
-
-  setTimeout(tick, speed);
-}
-
-function setNewFood() {
-  let place = [];
-  for (let a = 0; a < gridsize; a++) {
-    for (let b = 0; b < gridsize; b++) {
-      place.push(a + "-" + b);
-      for (let c = 0; c < poshistory.length; c++) {
-        if (poshistory[c][0] == a + "-" + b) {
-          place.pop();
-          //break;
-        }
-      }
-    }
-  }
-  food = place[Math.floor(Math.random() * place.length)];
-}
-
-
-//------------------------------------
-*/
